2b263eca90a7d338eabc3aa06f0f79c78ed7e1c4
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:58:36 +0530

    7. RemoveSpacesString

---
+++ 26.12.25/7. RemoveSpacesString.java
@@ -0,0 +1,6 @@
+public class RemoveSpacesString {
+    public static void main(String[] args) {
+        String s = "Java Programming Language";
+        System.out.println(s.replaceAll("\\s", ""));
+    }
+}
