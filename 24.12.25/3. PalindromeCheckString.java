995b7f0c8637592215cd889ad5322f4ec20379d7
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:37:29 +0530

    3. PalindromeCheckString

---
+++ 24.12.25/3. PalindromeCheckString.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckString {
+    public static void main(String[] args) {
+        String str="level";
+        String rev=new StringBuilder(str).reverse().toString();
+        System.out.println(str.equals(rev)?"Palindrome":"Not Palindrome");
+    }
+}
