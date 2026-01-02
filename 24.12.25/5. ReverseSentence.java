1b2e9214e1f824431441c4ed7d748e1e6c2222d3
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:38:01 +0530

    Create 5. ReverseSentence.java

---
+++ 24.12.25/5. ReverseSentence.java
@@ -0,0 +1,7 @@
+public class ReverseSentence {
+    public static void main(String[] args) {
+        String s="Java is powerful";
+        String[] words=s.split(" ");
+        for(int i=words.length-1;i>=0;i--) System.out.print(words[i]+" ");
+    }
+}
