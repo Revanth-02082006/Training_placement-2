b7ac2c40cd30dac0300fb44e47dab3748d853ecd
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:36:48 +0530

    1. SumOfSquares

---
+++ 24.12.25/1. SumOfSquares.java
@@ -0,0 +1,7 @@
+public class SumOfSquares {
+    public static void main(String[] args) {
+        int n=10,sum=0;
+        for(int i=1;i<=n;i++) sum+=i*i;
+        System.out.println("Sum of squares = "+sum);
+    }
+}
