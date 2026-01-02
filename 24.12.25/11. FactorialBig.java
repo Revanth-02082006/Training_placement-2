6f100fed5b6891f5798ac755a4b67c21254b7268
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:39:45 +0530

    Create 11. FactorialBig.java

---
+++ 24.12.25/11. FactorialBig.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+public class FactorialBig {
+    public static void main(String[] args) {
+        int n=20; BigInteger fact=BigInteger.ONE;
+        for(int i=1;i<=n;i++) fact=fact.multiply(BigInteger.valueOf(i));
+        System.out.println("Factorial = "+fact);
+    }
+}
