2c1ffb1058310901c589324ddd400f1abf3e40b1
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:50:46 +0530

    12. SumEvenDigits

---
+++ 25.12.25/12. SumEvenDigits.java
@@ -0,0 +1,11 @@
+public class SumEvenDigits {
+    public static void main(String[] args) {
+        int num = 48291, sum = 0;
+        while(num > 0){
+            int d = num % 10;
+            if(d % 2 == 0) sum += d;
+            num /= 10;
+        }
+        System.out.println("Sum of even digits = " + sum);
+    }
+}
