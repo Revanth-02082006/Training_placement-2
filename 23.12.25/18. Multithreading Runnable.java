69e09fcf9a3cd78cb19de84e8c1ca169d0475225
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:33:38 +0530

    18. Multithreading Runnable.java

--- 23.12.25/18. Multithreading Runnable.java
+++ 23.12.25/18. Multithreading Runnable.java
@@ -1,3 +1,22 @@
 class MyRun implements Runnable {
-    public void run(){
-        for(int i=1;i<=5;i++) System
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - Count: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted!");
+            }
+        }
+    }
+}
+
+public class ThreadRunnable {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyRun(), "Thread 1");
+        Thread t2 = new Thread(new MyRun(), "Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
