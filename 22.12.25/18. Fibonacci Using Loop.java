8fe47561a97ae0234209f6ab57837fa9af9e7001
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:23:00 +0530

    18. Fibonacci Using Loop

---
+++ 22.12.25/18. Fibonacci Using Loop.java
@@ -0,0 +1,6 @@
+public class FibonacciLoop {
+    public static void main(String[] args) {
+        int n=10,a=0,b=1;
+        for(int i=0;i<n;i++){ System.out.print(a+" "); int next=a+b; a=b; b=next; }
+    }
+}
