0a49b27ce7abf47025c6ba0ea0bf77f5560153b3
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:38:58 +0530

    8. PrimeSum

---
+++ 24.12.25/8. PrimeSum.java
@@ -0,0 +1,12 @@
+public class PrimeSum {
+    public static boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i=2;i<=Math.sqrt(n);i++) if(n%i==0) return false;
+        return true;
+    }
+    public static void main(String[] args){
+        int sum=0;
+        for(int i=2;i<=50;i++) if(isPrime(i)) sum+=i;
+        System.out.println("Sum of primes up to 50 = "+sum);
+    }
+}
