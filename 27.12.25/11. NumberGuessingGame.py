78090850f34cad340416cb7914a2c0034c1e97d9
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 11:13:42 +0530

    11. NumberGuessingGame

---
+++ 27.12.25/11. NumberGuessingGame.py
@@ -0,0 +1,11 @@
+import random
+
+num = random.randint(1, 100)
+guess = None
+while guess != num:
+    guess = int(input("Guess a number (1-100): "))
+    if guess < num:
+        print("Too low!")
+    elif guess > num:
+        print("Too high!")
+print("Correct! The number was", num)
