e17cda15517f8bf416331b8740281f0b728cf4ef
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:04:25 +0530

    12. Compound Interest

---
+++ 20.12.25/12. Compound Interest.java
@@ -0,0 +1,7 @@
+public class CompoundInterest {
+    public static void main(String[] args) {
+        double p=1000, r=5, t=2;
+        double ci = p * Math.pow((1 + r/100), t) - p;
+        System.out.println("Compound Interest = " + ci);
+    }
+}
