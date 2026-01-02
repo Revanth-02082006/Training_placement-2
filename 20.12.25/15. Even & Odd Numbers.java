4ecb56d6406856f497a9088de306b0a717104c2a
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:05:23 +0530

    15. Even & Odd Numbers

---
+++ 20.12.25/15. Even & Odd Numbers.java
@@ -0,0 +1,8 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i%2==0) System.out.println(i+" is Even");
+            else System.out.println(i+" is Odd");
+        }
+    }
+}
