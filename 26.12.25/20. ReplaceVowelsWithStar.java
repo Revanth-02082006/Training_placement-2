f2f108043f47d3878cb8b8a32a948bd7f35081d0
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 11:03:01 +0530

    20. ReplaceVowelsWithStar

---
+++ 26.12.25/20. ReplaceVowelsWithStar.java
@@ -0,0 +1,6 @@
+public class ReplaceVowelsWithStar {
+    public static void main(String[] args) {
+        String s="Programming";
+        System.out.println(s.replaceAll("[AEIOUaeiou]", "*"));
+    }
+}
