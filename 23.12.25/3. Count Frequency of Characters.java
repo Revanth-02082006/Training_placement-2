656e53a5cb3d74afc7f983fcb18349b61dc776e2
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:25:50 +0530

    3. Count Frequency of Characters

---
+++ 23.12.25/3. Count Frequency of Characters.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        HashMap<Character,Integer> freq = new HashMap<>();
+        for(char c: str.toCharArray())
+            freq.put(c, freq.getOrDefault(c,0)+1);
+        System.out.println(freq);
+    }
+}
