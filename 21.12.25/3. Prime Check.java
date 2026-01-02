abad22a2c89da41440244f30103f81e93ed71a57
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:10:19 +0530

    3. Prime Check

---
+++ 21.12.25/3. Prime Check.java
@@ -0,0 +1,10 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean prime = true;
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0){ prime=false; break; }
+        }
+        System.out.println(num + (prime?" is Prime":" is Not Prime"));
+    }
+}
