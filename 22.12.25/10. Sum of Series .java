7e97d7158487484a349c7ee328117c4fde7d2c85
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:20:41 +0530

    10. Sum of Series

---
+++ 22.12.25/10. Sum of Series .java
@@ -0,0 +1,7 @@
+public class SumSeries {
+    public static void main(String[] args) {
+        int n=5; double sum=0;
+        for(int i=1;i<=n;i++) sum+=1.0/i;
+        System.out.println("Sum of series = "+sum);
+    }
+}
