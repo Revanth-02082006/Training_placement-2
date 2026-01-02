7262240c4de08ae3a619f3a276647ab026a71f6a
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:52:29 +0530

    16. RemoveVowelsFromString

---
+++ 25.12.25/16. RemoveVowelsFromString.java
@@ -0,0 +1,6 @@
+public class RemoveVowelsFromString {
+    public static void main(String[] args) {
+        String s = "Programming";
+        System.out.println(s.replaceAll("[AEIOUaeiou]", ""));
+    }
+}
