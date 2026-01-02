c2fd2c62ab521645fd98658dc2c37dc0467aca59
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:20:08 +0530

    8. Sum of Matrix Diagonals

---
+++ 22.12.25/8. Sum of Matrix Diagonals.java
@@ -0,0 +1,8 @@
+public class MatrixDiagonalSum {
+    public static void main(String[] args) {
+        int[][] mat={{1,2,3},{4,5,6},{7,8,9}};
+        int sum=0;
+        for(int i=0;i<mat.length;i++) sum+=mat[i][i]+mat[i][mat.length-1-i];
+        System.out.println("Diagonal Sum = "+sum);
+    }
+}
