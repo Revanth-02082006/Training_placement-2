e90ed52a89a165139b4e22b4a6c80ee7c5dc9147
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:47:46 +0530

    8. ReplaceSubstring

---
+++ 25.12.25/8. ReplaceSubstring.java
@@ -0,0 +1,6 @@
+public class ReplaceSubstring {
+    public static void main(String[] args) {
+        String s = "I love Java programming";
+        System.out.println(s.replace("Java", "Python"));
+    }
+}
