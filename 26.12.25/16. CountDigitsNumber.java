afdb22e503a2f82bdd99fe2dfc625ec86b379a45
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 11:01:56 +0530

    16. CountDigitsNumber

---
+++ 26.12.25/16. CountDigitsNumber.java
@@ -0,0 +1,8 @@
+public class CountDigitsNumber {
+    public static void main(String[] args) {
+        int num=98765;
+        int count=0;
+        while(num>0){ count++; num/=10; }
+        System.out.println("Digits = "+count);
+    }
+}
