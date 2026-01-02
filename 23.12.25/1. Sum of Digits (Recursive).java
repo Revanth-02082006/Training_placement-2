6ea9309fefb295755b0c5ae0cbd2dafa8ef66956
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:25:12 +0530

    1. Sum of Digits (Recursive)

---
+++ 23.12.25/1. Sum of Digits (Recursive).java
@@ -0,0 +1,10 @@
+public class SumDigitsRecursive {
+    public static int sumDigits(int n) {
+        if (n == 0) return 0;
+        return (n % 10) + sumDigits(n / 10);
+    }
+    public static void main(String[] args) {
+        int num = 987;
+        System.out.println("Sum of digits = " + sumDigits(num));
+    }
+}
