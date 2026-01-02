189dc5d13309e68178655a1e420cadd0bee7b5e9
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:53:23 +0530

    19. CountEvenOddArray

---
+++ 25.12.25/19. CountEvenOddArray.java
@@ -0,0 +1,10 @@
+public class CountEvenOddArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int even=0, odd=0;
+        for(int n:arr){
+            if(n%2==0) even++; else odd++;
+        }
+        System.out.println("Even="+even+", Odd="+odd);
+    }
+}
