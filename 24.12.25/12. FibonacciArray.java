c1af6db67d0ad877e5a0fe00e7e586d0130aa6eb
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-02 10:40:00 +0530

    12. FibonacciArray

---
+++ 24.12.25/12. FibonacciArray.java
@@ -0,0 +1,8 @@
+public class FibonacciArray {
+    public static void main(String[] args) {
+        int n=10; int[] fib=new int[n];
+        fib[0]=0; fib[1]=1;
+        for(int i=2;i<n;i++) fib[i]=fib[i-1]+fib[i-2];
+        for(int f:fib) System.out.print(f+" ");
+    }
+}
