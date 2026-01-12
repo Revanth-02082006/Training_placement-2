40711f54e962a46f363d0492bb911c22ec1bf9c1
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-12 10:34:29 +0530

    3. Random Sentence Generator

---
+++ 12.1.26/3. Random Sentence Generator.py
@@ -0,0 +1,5 @@
+import random
+subj=["cats","robots","dreams","clouds"]
+verb=["dance","whisper","glow","wander"]
+obj=["silently","forever","in circles","with joy"]
+print(random.choice(subj),random.choice(verb),random.choice(obj))
