a45194228e7fa09f62a8cf0543224f9f9a321fff
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-12 10:38:10 +0530

    15. Random Story Generator

---
+++ 12.1.26/15. Random Story Generator.py
@@ -0,0 +1,5 @@
+import random
+c=["wizard","pirate","robot"]
+p=["forest","ocean","city"]
+a=["found treasure","cast a spell","built a machine"]
+print(f"A {random.choice(c)} in the {random.choice(p)} {random.choice(a)}.")
