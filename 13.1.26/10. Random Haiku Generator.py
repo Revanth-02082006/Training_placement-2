bb24bb6972579ab8973dd8d6cf721570ca57ff1a
Revanth-02082006 <revanthsathya.ind@gmail.com>
2026-01-13 10:04:48 +0530

    10. Random Haiku Generator

---
+++ 13.1.26/10. Random Haiku Generator.py
@@ -0,0 +1,7 @@
+import random
+
+five=["Silent pond","Winter moon","Lonely crow"]
+seven=["Whispers through the forest","Echoes of forgotten dreams"]
+print(random.choice(five))
+print(random.choice(seven))
+print(random.choice(five))
