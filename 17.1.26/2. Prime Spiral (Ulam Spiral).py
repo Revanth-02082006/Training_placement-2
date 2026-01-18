2f980e42b2ca844586d872080dc4feec3d9050ca
Revanth A S <revanthsathya.ind@gmail.com>
2026-01-18 14:14:58 +0530

    2. Prime Spiral (Ulam Spiral)

---
+++ 17.1.26/2. Prime Spiral (Ulam Spiral).py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0: return False
+    return True
+
+size = 50
+x, y = 0, 0
+dx, dy = 0, -1
+points = []
+for i in range(1, size**2+1):
+    if is_prime(i):
+        points.append((x, y))
+    if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+        dx, dy = -dy, dx
+    x, y = x+dx, y+dy
+
+plt.scatter(*zip(*points), s=10)
+plt.show()
