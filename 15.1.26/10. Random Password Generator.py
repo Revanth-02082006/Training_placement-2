930016026fa689cbde3b7be48a4a7b624c300b72
Revanth A S <revanthsathya.ind@gmail.com>
2026-01-18 14:30:52 +0530

    10. Random Password Generator

---
+++ 15.1.26/10. Random Password Generator.py
@@ -0,0 +1,7 @@
+import random, string
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*"
+    return "".join(random.choice(chars) for _ in range(length))
+
+print(generate_password())
