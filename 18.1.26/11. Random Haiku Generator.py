dbfc2fc22d994e9c3aea60334e90d1f40524261e
Revanth A S <revanthsathya.ind@gmail.com>
2026-01-18 14:37:33 +0530

    11. Random Haiku Generator

---
+++ 18.1.26/11. Random Haiku Generator.py
@@ -0,0 +1,5 @@
+import random
+
+five = ["Silent moonlight", "Winter winds blow", "Coding all night"]
+seven = ["Dreams drift across the valley", "Errors vanish in the mist", "Ideas bloom endlessly"]
+print(f"{random.choice(five)}\n{random.choice(seven)}\n{random.choice(five)}")
