c2c40d85da766ad781f7a5630f3a24ef165682ae
Revanth A S <revanthsathya.ind@gmail.com>
2026-03-05 16:52:25 +0530

    stdin and stdout
    Implement a Java program to read and print three integers from standard input.

---
+++ 4.3.26/Stdin and stdout.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();   // first integer
+        int b = scan.nextInt();   // second integer
+        int c = scan.nextInt();   // third integer
+        
+        scan.close(); // always good practice to close the scanner
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
